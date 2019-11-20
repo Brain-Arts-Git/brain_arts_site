95aa9ec9e3ecfeb9f40b6c2b27652737959e9024
Alexander Keklak <alexanderkeklak@Alexanders-MacBook-Pro.local>
2019-11-19 23:40:00 -0500

    Made brain arts logo into link, added shows section

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,18 @@ from app import app
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-	return render_template('index.html') #add arguments here
+@app.route('/home')
+def home():
+	shows = [
+		{
+			'artist': 'Bon Iver',
+			'location': 'TD Garden',
+			'date': '11/20/19'
+		},
+		{
+			'artist': '''Margot and the Nuclear So and So's''',
+			'location': 'TD Garden',
+			'date': '11/25/29'
+		}
+	]
+	return render_template('home.html', shows=shows) #add arguments here
