8ca924a8d72aa95c5b5fd4bdb3fdaf355fba4d36
Alexander Keklak <alexanderkeklak@Alexanders-MacBook-Pro.local>
2019-11-20 19:34:57 -0500

    Added font folder to static dir, and linked in css file.  Now is futura-medium

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #prevents caching of static files
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -4,17 +4,23 @@ from app import app
 
 @app.route('/')
 @app.route('/home')
+@app.route('/index')
 def home():
-	shows = [
-		{
-			'artist': 'Bon Iver',
-			'location': 'TD Garden',
-			'date': '11/20/19'
-		},
-		{
-			'artist': '''Margot and the Nuclear So and So's''',
-			'location': 'TD Garden',
-			'date': '11/25/29'
-		}
-	]
-	return render_template('home.html', shows=shows) #add arguments here
+    """ 
+    this should turn into a call to a function that reads a text file with the 
+    shows and then returns them in the following list of dicts format, or just
+    a call to a database. 
+    """
+    shows = [
+        {
+            'artist': 'Bon Iver',
+            'location': 'TD Garden',
+            'date': '11/20/19'
+        },
+        {
+            'artist': '''Margot and the Nuclear So and So's''',
+            'location': 'TD Garden',
+            'date': '11/25/29'
+        }
+    ]
+    return render_template('home.html', shows=shows) #add arguments here
